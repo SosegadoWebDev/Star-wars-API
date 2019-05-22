b39ecf2c6c5fa56c0222a59f0ba33ca4672e9a11
Jaime González <arcarius11@gmail.com>
2019-05-22 09:13:40 -0300

    añadiendo script en personajes.html

--- scripts.js
+++ scripts.js
@@ -1,9 +1,7 @@
 $(document).ready(function () {
     console.log("jquery funcionando...")
-    //verificar input email
     var resultados = []
-
-
+    //verificar input email
     var inputEmail = $("#input-email")
     var messageErrorEmail = $(".message-error-email")
     inputEmail.on("input", function () {
@@ -80,34 +78,56 @@ $(document).ready(function () {
             })
     }
     function añadirPeople(info) {
-        
-        resultados = info.results
+        //resultados = !(JSON.parse(localStorage.getItem("Personajes-Star-Wars"))) ? resultados = [] : resultados = JSON.parse(localStorage.getItem("Personajes-Star-Wars"))
+        resultados.push(info.results)
         for (var i = 0; i < info.results.length; i++) {
-                $("tbody").append("<tr>" + "<th>" + $("tr").length + "</th>" + "<th>" + info.results[i].name + "</th>" + "<th>" + info.results[i].gender + "</th>" + "<th>" + info.results[i].height + "</th>" + "<th>" + info.results[i].mass + "</th>" + "<th>" + info.results[i].eye_color + "</th>" + "<th>" + "<button type='button' id='"+($("tr").length)+"' class='btn btn-success button-save'>" + "Guardar" + "</button>" + "</th>" + "</tr>")
+            $("tbody").append("<tr>" + "<th>" + $("tr").length + "</th>" + "<th>" + info.results[i].name + "</th>" + "<th>" + info.results[i].gender + "</th>" + "<th>" + info.results[i].height + "</th>" + "<th>" + info.results[i].mass + "</th>" + "<th>" + info.results[i].eye_color + "</th>" + "<th>" + "<button type='button' id='" + ($("tr").length) + "' class='btn btn-success button-save'>" + "Guardar" + "</button>" + "</th>" + "</tr>")
         }
     }
     $(".button-characters").on("click", function (event) {
         var timesClicked = event.target.id
         if (timesClicked <= 8) {
-        var numberId = (Number(timesClicked) + 1)
-        var url = "https://swapi.co/api/people/?page=" + numberId
-        $(".button-characters")[0].id = numberId
-        getData(url, añadirPeople) 
-            if(timesClicked == 8) {
+            var numberId = (Number(timesClicked) + 1)
+            var url = "https://swapi.co/api/people/?page=" + numberId
+            $(".button-characters")[0].id = numberId
+            getData(url, añadirPeople)
+            if (timesClicked == 8) {
                 $(".button-characters").attr("disabled", true)
             }
         }
     })
     //functión button save
-    $(".container").on("click", ".button-save", function(event){
+    function guardarEnLocalStorage(key, array) {
+        if (true) {
+           var CharactersList = !(JSON.parse(localStorage.getItem("Personajes-Star-Wars"))) ? CharactersList = [] : CharactersList = JSON.parse(localStorage.getItem("Personajes-Star-Wars"))
+           CharactersList.push(array)
+           var charJson = JSON.stringify(CharactersList)
+            localStorage.setItem(key, charJson)
+        }
+    }
+    function levantarStorage(key) {
+        if (localStorage.getItem(key)) {
+            var showCharacters = JSON.parse(localStorage.getItem(key))
+            console.log(showCharacters)
+        }
+    }
+    $(".container").on("click", ".button-save", function (event) {
         var captureIdToSave = event.target.id
-    
-        //var $btn = $(event.target)
-        console.log(resultados)
-        console.log(resultados[captureIdToSave])
-
-        //console.log($("tr")[captureIdToSave].childNodes[0].innerText)
-        
+        var saveChar = {
+            name: $("tr")[captureIdToSave].children[1].textContent,
+            gender: $("tr")[captureIdToSave].children[2].textContent,
+            height: $("tr")[captureIdToSave].children[3].textContent,
+            mass: $("tr")[captureIdToSave].children[4].textContent,
+            eye_color: $("tr")[captureIdToSave].children[5].textContent
+        }
+        $("#"+ captureIdToSave).attr("disabled", true)
+        for (var i = 0; i < resultados.length; i++) {
+            for (var ii = 0; ii < resultados[i].length; ii++) {
+                if (resultados[i][ii].name == ($("tr")[captureIdToSave].children[1].textContent)) {
+                    guardarEnLocalStorage("Personajes-Star-Wars", saveChar)
+                }
+            }
+        }
     })
     //termina el ready()
 })
